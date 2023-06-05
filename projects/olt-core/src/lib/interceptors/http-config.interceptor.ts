ecdb64eb0c3ad398797a4d81a0fb030fb1075ed3
Chris Straw <chris.straw@outerlimitstech.com>
2023-06-05 09:12:26 -0400

    15.0.0-beta.3

--- projects/olt-core/src/lib/interceptors/http-config.interceptor.ts
+++ projects/olt-core/src/lib/interceptors/http-config.interceptor.ts
@@ -15,43 +15,14 @@ export abstract class OltHttpConfigInterceptor implements HttpInterceptor {
     protected router: Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    const isSmartyStreets = request.url.indexOf('smartystreets.com') > -1;
-    if (!isSmartyStreets) {
-      if (this.authService.isAuthenticated && this.authService.token != null) {
-        const tokenType = this.authService.tokenType || 'Bearer';
-        request = request.clone({
-          setHeaders: {
-            Authorization: `${tokenType} ${this.authService.token}`
-          }
-        });
-      }
-
-      request = request.clone({
-        setHeaders: {
-          'Access-Control-Allow-Origin': '*',
-        }
-      });
-
-      const url = this.configService.apiUrl(request.url);
-      request = request.clone({ url });
-    }
-
+    request = this.configureBearer(request);
+    request = this.configureCORS(request);
+    request = this.configureApiUrl(request);
 
     return next.handle(request).pipe(
-      // map((event: HttpEvent<any>) => {
-      //   if (event instanceof HttpResponse) {
-      //     // console.log('event--->>>', event);
-      //     // this.errorDialogService.openDialog(event);
-      //   }
-      //   return event;
-      // }),
 
       catchError((error: HttpErrorResponse) => {
-        // if (error.status === HttpStatusCode.BadRequest && error.error && error.error.validationErrors && error.error.validationErrors.length > 0) {
-        //   // this._modalService.showValidationModal(err.error.validationErrors);
-        //   return throwError(error);
-        // }
+
 
         if (error.status === HttpStatusCode.Unauthorized) {
           if (this.configService.isProduction !== true) {
@@ -71,5 +42,33 @@ export abstract class OltHttpConfigInterceptor implements HttpInterceptor {
 
 
   }
+
+
+  protected configureApiUrl(request: HttpRequest<any>): HttpRequest<any> {
+    const url = this.configService.apiUrl(request.url);
+    return request.clone({ url });
+  }
+
+  protected configureBearer(request: HttpRequest<any>): HttpRequest<any> {
+    if (this.authService.isAuthenticated && this.authService.token != null) {
+      const tokenType = this.authService.tokenType || 'Bearer';
+      return request.clone({
+        setHeaders: {
+          Authorization: `${tokenType} ${this.authService.token}`
+        }
+      });
+    }
+    return request;
+  }
+
+  protected configureCORS(request: HttpRequest<any>): HttpRequest<any> {
+    // return request.clone({
+    //   setHeaders: {
+    //     'Access-Control-Allow-Origin': '*',
+    //   }
+    // });
+    return request;
+  }
+
 
 }
