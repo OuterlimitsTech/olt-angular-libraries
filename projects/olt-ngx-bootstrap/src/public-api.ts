2816cc68ccdf3e5cbc93598ce067667161037851
Chris Straw <chris.straw@outerlimitstech.com>
2023-07-07 07:33:55 -0400

    created olt-ngx-bootstrap

---
+++ projects/olt-ngx-bootstrap/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of olt-ngx-bootstrap
+ */
+
+export * from './lib/olt-ngx-bootstrap.service';
+export * from './lib/olt-ngx-bootstrap.component';
+export * from './lib/olt-ngx-bootstrap.module';
