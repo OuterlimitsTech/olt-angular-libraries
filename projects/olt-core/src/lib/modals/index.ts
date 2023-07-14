5daf7722d4b2e04f9a591097c26f5e61fc364018
Chris Straw <chris.straw@outerlimitstech.com>
2023-07-14 19:30:00 -0400

    removed old components from 15.x olt core lib

--- projects/olt-core/src/lib/modals/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './base-modal.component';
-export * from './input-modal/input-modal.component';
-export * from './confirm-modal/confirm-modal.component';
-export * from './alert-modal/alert-modal.component';
-
